ec3022de28627293bdbd2cfedc08ad7166e5cf19
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2025-07-15 22:54:05 -0500

    feat: remove server setup code from app.js

--- server/app.js
+++
@@ -1,9 +0,0 @@
-import { config } from '@/config.js'
-import { router } from '@/routes/setup.js'
-
-export const server = globalThis.Bun.serve({
-  port: config.server.port,
-  fetch: router,
-})
-
-console.log(`🚀 Server running on http://localhost:${config.server.port}`)
